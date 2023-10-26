3aed5a68163d97abdeffb3779b3bee1e9d5627c1
Christian David <christiandavid@Christians-MacBook-Pro.local>
2023-10-26 08:39:53 -0400

    take 3

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+
 const menuLinks = [
     {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
   ];
 
 const mainEl = document.querySelector("main")
@@ -26,4 +38,48 @@ menuLinks.forEach(link =>{
     topMenuEl.appendChild( newMenuLinks)
     
 })
-console.dir(menuLinks)
+// END OF EXCERCISE PART 1
+const subMenuEl = document.querySelector("#sub-menu")
+subMenuEl.style.height="100%"
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)"
+subMenuEl.classList.add("flex-around")
+subMenuEl.style.position = "absolute"
+subMenuEl.style.top= "0"
+
+const topMenuLinks = topMenuEl.querySelectorAll("a")
+console.dir(topMenuLinks)
+
+
+// 
+topMenuEl.addEventListener("click",(event) =>{
+   event.preventDefault()
+   console.log(event.target.textContent)
+    if (event.target.tagName!=="A"){
+        console.log("A")
+        
+
+        return
+    }
+//5.5
+    event.target.classList.add("active")
+// end 5.5 
+    if (event.target.classList.contains('active')) {
+        event.target.classList.remove('active');
+        showingSubMenu="false"
+        subMenuEl.style.top="0"
+        return;
+
+    }
+    // end of 5.3
+    // 5.4
+   topMenuLinks.forEach((link) => {
+    link.classList.remove('active');
+
+   });
+// end 5.4
+showingSubMenu = menuLinks[topMenuLinks.indexOf(event.target)].hasOwnProperty("subLinks")
+console.log(showingSubMenu)
+} )
+
+//let showingSubMenu = false
+//subMenuEl.style.top="0"
